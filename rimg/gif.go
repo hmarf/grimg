e2300a364acd6436a24f0c21f6d40c0453c9436d
hmarf <hmarfUni@gmail.com>
2019-12-21 11:12:54 +0900

    希望が見えた

--- rimg/gif.go
+++ rimg/gif.go
@@ -48,28 +48,19 @@ func (g *GifService) Resize(width uint, height uint, rate float64) error {
 		scUsedP := keys(cUsedM)
 		rrec := rImage.Bounds()
 		if i > 0 {
-			x := int(width)  //int(math.Floor(float64(rec.Min.X) * rate))
-			y := int(height) //int(math.Floor(float64(rec.Min.Y) * rate))
-			rrec = image.Rect(x, y, rrec.Dx()+x, rrec.Dy()+y)
+			x := int(width)
+			y := int(height)
+			rrec = image.Rect(0, 0, x, y)
 		}
 		rp := image.NewPaletted(rrec, scUsedP)
 		draw.Draw(rp, rrec, rImage, image.ZP, draw.Src)
 
-		out, err := os.Create("resized.png")
-		if err != nil {
-			return err
-		}
-		defer out.Close()
-
-		png.Encode(out, rp)
-
 		outGif.Image = append(outGif.Image, rp)
-		outGif.Delay = append(outGif.Delay, 0)
+		outGif.Delay = append(outGif.Delay, 100)
 	}
 
 	outGif.Config.Width = int(width)
 	outGif.Config.Height = int(height)
-	fmt.Print(outGif.Config.Width, outGif.Config.Height)
 
 	out, err := os.Create("resized.gif")
 	if err != nil {
@@ -77,8 +68,6 @@ func (g *GifService) Resize(width uint, height uint, rate float64) error {
 	}
 	defer out.Close()
 
-	// var gifImage gif.GIF
-	// gifImage = *g.Img
 	err = gif.EncodeAll(out, &outGif)
 	fmt.Println(err)
 	fmt.Println("ok")
