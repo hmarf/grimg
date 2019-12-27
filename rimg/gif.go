cf6d05d9782d5913541e79c46e395715aca8aed0
hmarf <hmarfUni@gmail.com>
2019-12-28 01:44:19 +0900

    Test whether Gif image generation only with difference image by encodeAll works well

--- rimg/gif.go
+++ rimg/gif.go
@@ -2,14 +2,9 @@ package rimg
 
 import (
 	"fmt"
-	"image"
 	"image/color"
-	"image/draw"
 	"image/gif"
-	"math"
 	"os"
-
-	"github.com/nfnt/resize"
 )
 
 type gifService struct {
@@ -34,36 +29,37 @@ func (g *gifService) resize(width uint, height uint, o Option) error {
 	fmt.Println()
 	for i, frame := range g.Img.Image {
 
-		rec := frame.Bounds()
+		// rec := frame.Bounds()
 
 		// rImage := resize.Resize(width, height, frame.SubImage(rec), resize.Lanczos3)
-		rImage := resize.Resize(
-			uint(math.Floor(float64(rec.Dx())*o.Compression)),
-			uint(math.Floor(float64(rec.Dy())*o.Compression)),
-			frame.SubImage(rec), resize.Lanczos3)
-		rrec := rImage.Bounds()
+		// rImage := resize.Resize(
+		// 	uint(math.Floor(float64(rec.Dx())*o.Compression)),
+		// 	uint(math.Floor(float64(rec.Dy())*o.Compression)),
+		// 	frame.SubImage(rec), resize.Lanczos3)
+		// rrec := rImage.Bounds()
 
-		cUsedM := make(map[color.Color]bool)
-		// The color used in the original image
-		for x := 1; x <= rec.Dx(); x++ {
-			for y := 1; y <= rec.Dy(); y++ {
-				if _, ok := cUsedM[frame.At(x, y)]; !ok {
-					cUsedM[frame.At(x, y)] = true
-				}
-			}
-		}
-		scUsedP := keys(cUsedM)
-		if i > 0 {
-			rrec = image.Rect(
-				int(math.Floor(float64(rec.Min.X)*o.Compression)),
-				int(math.Floor(float64(rec.Min.Y)*o.Compression)),
-				rrec.Dx()+int(math.Floor(float64(rec.Min.X)*o.Compression)),
-				rrec.Dy()+int(math.Floor(float64(rec.Min.Y)*o.Compression)))
-		}
+		// cUsedM := make(map[color.Color]bool)
+		// // The color used in the original image
+		// for x := 1; x <= rec.Dx(); x++ {
+		// 	for y := 1; y <= rec.Dy(); y++ {
+		// 		if _, ok := cUsedM[frame.At(x, y)]; !ok {
+		// 			cUsedM[frame.At(x, y)] = true
+		// 		}
+		// 	}
+		// }
+		// scUsedP := keys(cUsedM)
+		// if i > 0 {
+		// 	rrec = image.Rect(
+		// 		int(math.Floor(float64(rec.Min.X)*o.Compression)),
+		// 		int(math.Floor(float64(rec.Min.Y)*o.Compression)),
+		// 		rrec.Dx()+int(math.Floor(float64(rec.Min.X)*o.Compression)),
+		// 		rrec.Dy()+int(math.Floor(float64(rec.Min.Y)*o.Compression)))
+		// }
 
-		rp := image.NewPaletted(rrec, scUsedP)
-		draw.Draw(rp, rrec, rImage, image.ZP, draw.Src)
-		outGif.Image = append(outGif.Image, rp)
+		// rp := image.NewPaletted(rec, scUsedP)
+		// draw.Draw(rp, rec, frame, image.ZP, draw.Src)
+
+		outGif.Image = append(outGif.Image, frame)
 		outGif.Delay = append(outGif.Delay, g.Img.Delay[i])
 		fmt.Println(g.Img.Disposal[i])
 		outGif.Disposal = append(outGif.Disposal, g.Img.Disposal[i])
