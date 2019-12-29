3359c60c1e325b19b96740bb0dca888533ff4b60
hmarf <hmarfUni@gmail.com>
2019-12-30 00:29:08 +0900

    [失敗] 画像を合成してからリサイズ

--- rimg/gif.go
+++ rimg/gif.go
@@ -2,9 +2,14 @@ package rimg
 
 import (
 	"fmt"
+	"image"
 	"image/color"
+	"image/draw"
 	"image/gif"
+	"math"
 	"os"
+
+	"github.com/nfnt/resize"
 )
 
 type gifService struct {
@@ -27,39 +32,65 @@ func (g *gifService) resize(width uint, height uint, o Option) error {
 	//cUsedM := make(map[color.Color]bool)
 	fmt.Println(g.Img.Disposal[0])
 	fmt.Println()
+	var pImage image.Image
 	for i, frame := range g.Img.Image {
 
-		// rec := frame.Bounds()
+		rec := frame.Bounds()
 
 		// rImage := resize.Resize(width, height, frame.SubImage(rec), resize.Lanczos3)
-		// rImage := resize.Resize(
-		// 	uint(math.Floor(float64(rec.Dx())*o.Compression)),
-		// 	uint(math.Floor(float64(rec.Dy())*o.Compression)),
-		// 	frame.SubImage(rec), resize.Lanczos3)
-		// rrec := rImage.Bounds()
+		var rImage image.Image
+		if i > 0 {
+			// 書き出し用のイメージを準備
+			outRect := image.Rectangle{image.Pt(0, 0), rec.Size()}
+			out := image.NewRGBA(outRect)
 
-		// cUsedM := make(map[color.Color]bool)
-		// // The color used in the original image
-		// for x := 1; x <= rec.Dx(); x++ {
-		// 	for y := 1; y <= rec.Dy(); y++ {
-		// 		if _, ok := cUsedM[frame.At(x, y)]; !ok {
-		// 			cUsedM[frame.At(x, y)] = true
-		// 		}
-		// 	}
-		// }
-		// scUsedP := keys(cUsedM)
-		// if i > 0 {
-		// 	rrec = image.Rect(
-		// 		int(math.Floor(float64(rec.Min.X)*o.Compression)),
-		// 		int(math.Floor(float64(rec.Min.Y)*o.Compression)),
-		// 		rrec.Dx()+int(math.Floor(float64(rec.Min.X)*o.Compression)),
-		// 		rrec.Dy()+int(math.Floor(float64(rec.Min.Y)*o.Compression)))
-		// }
+			// 描画する
+			// 元画像をまず描く
+			dstRect := image.Rectangle{image.Pt(0, 0), rec.Size()}
+			draw.Draw(out, dstRect, pImage, image.Pt(0, 0), draw.Src)
 
-		// rp := image.NewPaletted(rec, scUsedP)
-		// draw.Draw(rp, rec, frame, image.ZP, draw.Src)
+			// 上書きする
+			srcRect := image.Rectangle{image.Pt(0, 0), rec.Size()}
+			draw.Draw(out, srcRect, frame, image.Pt(0, 0), draw.Over)
 
-		outGif.Image = append(outGif.Image, frame)
+			pImage = out
+			rImage = resize.Resize(
+				uint(math.Floor(float64(rec.Dx())*o.Compression)),
+				uint(math.Floor(float64(rec.Dy())*o.Compression)),
+				out, resize.Lanczos3)
+		} else {
+			rImage = resize.Resize(
+				uint(math.Floor(float64(rec.Dx())*o.Compression)),
+				uint(math.Floor(float64(rec.Dy())*o.Compression)),
+				frame.SubImage(rec), resize.Lanczos3)
+			pImage = frame
+		}
+
+		rrec := rImage.Bounds()
+		fmt.Println(rrec)
+
+		cUsedM := make(map[color.Color]bool)
+		// The color used in the original image
+		for x := 1; x <= rec.Dx(); x++ {
+			for y := 1; y <= rec.Dy(); y++ {
+				if _, ok := cUsedM[frame.At(x, y)]; !ok {
+					cUsedM[frame.At(x, y)] = true
+				}
+			}
+		}
+		scUsedP := keys(cUsedM)
+		if i > 0 {
+			rrec = image.Rect(
+				int(math.Floor(float64(rec.Min.X)*o.Compression)),
+				int(math.Floor(float64(rec.Min.Y)*o.Compression)),
+				rrec.Dx()+int(math.Floor(float64(rec.Min.X)*o.Compression)),
+				rrec.Dy()+int(math.Floor(float64(rec.Min.Y)*o.Compression)))
+		}
+
+		rp := image.NewPaletted(rrec, scUsedP)
+		draw.Draw(rp, rrec, rImage, image.ZP, draw.Src)
+
+		outGif.Image = append(outGif.Image, rp)
 		outGif.Delay = append(outGif.Delay, g.Img.Delay[i])
 		fmt.Println(g.Img.Disposal[i])
 		outGif.Disposal = append(outGif.Disposal, g.Img.Disposal[i])
